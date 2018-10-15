44ae34e5ff8908de11b922c8a8b9cb87b652bb33
BrotherWolf8 <daniel.w.innes@gmail.com>
2018-10-15 13:00:02 -0400

    part 2 done

--- Lab4Part2/src/SubClass.java
+++ Lab4Part2/src/SubClass.java
@@ -9,7 +9,7 @@ public class SubClass extends MyClass{
 		return b;
 	}
 	public int getA() {
-		return super.getA();
+		return super.getA()*2;
 	}
 	public String toString() {
 		return super.toString() + " b = " + b;
